3d2323e01c0960a80426960c6d97daa5c40007e1
Levi Gist <ligist2@gmail.com>
2023-04-02 21:58:42 -0500

    Videos Component file

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
 import SideBar from './SideBar';
+import Videos from './Videos';
 
 const Feed = () => {
   return (
     <Stack sx={{ flexDirection: {sx: "column", md: 'row'} }} >
-      <Box sx={{ height: { sx: 'auot', md: '92vh'}, borderRight: '1px solid #3d3d3d', px: {sx: 0, md: 2} }} >
+      <Box sx={{ height: { sx: 'auto', md: '92vh'}, borderRight: '1px solid #3d3d3d', px: {sx: 0, md: 2} }} >
         <SideBar />
 
         <Typography className='copyright' variant='body2' sx={{ mt: 1.5, color: '#FFF'}} >
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -5,7 +5,7 @@ import { categories } from '../utils/constants';
 const selectedCategory = 'New';
 
 const SideBar = () => (
-    <Stack direction='row' sx={{ overflowY: 'auot', height: {sx: 'auto', md: '95%'}, flexDirection: { md: 'column'} }} >
+    <Stack direction='row' sx={{ overflowY: 'auto', height: {sx: 'auto', md: '95%'}, flexDirection: { md: 'column'} }} >
         {categories.map((category) => (
             <button className='category-btn' style={{ background: category.name === selectedCategory && '#FC1503', color: 'white'}} key={category.name} >
                 <span style={{ color: category.name === selectedCategory ? 'white' : 'red' , marginRight: '15px'}} >
